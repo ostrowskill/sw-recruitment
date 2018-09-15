a31afb1f18f208972f7829e43eb5e32aa536f04e
Łukasz Ostrowski <l.ostrowski.x@gmail.com>
2018-09-15 16:52:59 +0200

    first validation stuff and so on

--- js/main.js
+++ js/main.js
@@ -12,9 +12,25 @@
     // pick card function
     const pickCard = (section, card, resetBg) => {
         section.querySelectorAll(card).forEach(
-            elem =>  elem.style.background = resetBg   
+            elem => {
+                elem.style.background = resetBg;
+                elem.querySelector('.btn').textContent = 'pick';
+
+            }
+
         );
     };
+
+    class selectedCard {
+        constructor(name, src) {
+            this.name = name;
+            this.src = src;
+        }
+    };
+
+    // is picked?
+    let pickedTrooper;
+    let pickedPlanet;
 
     // pick trooper
     const troopers = document.querySelector('.troopers');
@@ -22,7 +38,17 @@
     troopers.addEventListener('click', e => {
         if (e.target.nodeName === 'A') {
             pickCard(troopers, '.card', '#080d1b');
-            e.target.parentElement.style.background = '#0d1327';
+            e.target.textContent = 'picked';
+            const card = e.target.parentElement;
+            card.style.background = '#0d1327';
+            
+            //get info
+            const src= card.querySelector('img').src;
+            const name = card.querySelector('h2').innerHTML;
+
+            //pick action
+            pickedTrooper = new selectedCard(name, src);
+
         };
     });
 
@@ -31,8 +57,38 @@
     planets.addEventListener('click', e => {
         if (e.target.nodeName === 'A') {
             pickCard(planets, '.card', '#ded4c2');
-            e.target.parentElement.style.background = '#e7e1d4'
+            e.target.textContent = 'picked';
+            const card = e.target.parentElement;
+            card.style.background = '#e7e1d4'
+            //get info
+            const src= card.querySelector('img').src;
+            const name = card.querySelector('h2').innerHTML;
+            //pick action
+            pickedPlanet = new selectedCard(name, src);
         }
+    });
+
+
+    // contact form
+    const form = document.forms.contactform;
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const userName = form.querySelector('#username').value;
+        const userEmail = form.querySelector('#email').value;
+
+        if (pickedTrooper && pickedPlanet && userName && userEmail) {
+            console.log(pickedTrooper);
+            console.log(pickedPlanet);
+            console.log(userName);
+            console.log(userEmail);
+            const newCard = document.createElement('ul');
+            const piture = document.createElement('li.new-card');
+
+        } else {
+            console.log('something is not picked');
+        }
+
     });
 
     // other
